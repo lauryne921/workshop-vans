1c94af23a59b5b2785d9fa5d3139968081803686
lauryne <lauryne921@live.fr>
2024-07-03 13:00:04 +0200

    add instruction to start the project

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,18 +6,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
 </head>
-<body class="bg-blue-500 min-h-screen flex items-center justify-center">
+<body class="bg-black min-h-screen flex items-center justify-center">
   <main class="bg-white p-8 rounded shadow-md flex flex-col md:flex-row w-full max-w-4xl">
     <div class="w-full md:w-1/2 flex items-center justify-center mb-4 md:mb-0">
       <img class="affiche mx-auto" src="{{ asset('images/affiche_dalle.webp') }}" alt="Example Image">
     </div>
     <div class="w-full md:w-1/2">
       <div class="text-center mb-6">
-        <h1 class="text-2xl font-bold">Inscrivez-vous à l'évènement</h1>
+        <h1 class="text-4xl font-bold">Inscrivez-vous à l'évènement</h1>
       </div>
 
-      <p>Inscrivez-vous à notre évènement et tentez de remporter une de nos 200 paires exclusives à ces Jeux Olympiques 2024 de Paris.</p>
-      <p>Rejoignez-nous à Place de la Concorde et rencontrez de nombreux autres passionés comme vous.</p><br>
+      <p class="text-xs">Inscrivez-vous à notre évènement et tentez votre chance d'être tiré au sort pour repartir avec une de nos 200 paires exclusives à ces Jeux Olympiques 2024 de Paris.</p>
+      <p class="text-xs">Vous recevrez un mail dans quelques jours pour connaitre la réponse.</p><br>
 
       @if(session('success'))
         <div class="bg-green-100 text-green-800 p-4 mb-4 rounded">
@@ -49,7 +49,7 @@
           <label class="block text-gray-700">Email :</label>
           <input type="email" name="email" value="{{ old('email') }}" required class="bg-gray-100 border border-gray-300 rounded w-full p-2 mt-1">
         </div>
-        <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded w-full">S'inscrire</button>
+        <button type="submit" class="bg-black text-white py-2 px-4 mt-2.5 rounded w-full">S'inscrire</button>
       </form>
     </div>
   </main>
