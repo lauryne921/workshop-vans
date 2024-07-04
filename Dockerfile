543fc3b3d6121a7c6e20e8ea9a8dda432a797b54
lauryne <lauryne921@live.fr>
2024-07-04 11:10:26 +0200

    update dockerfile and setpup.sh

--- Dockerfile
+++ Dockerfile
@@ -33,6 +33,8 @@ RUN chown -R www-data:www-data \
         /var/www/storage \
         /var/www/bootstrap/cache
 
+RUN npm install
+
 EXPOSE 80
 
 CMD ["apache2-foreground"]
--- setup.sh
+++ setup.sh
@@ -12,4 +12,9 @@ docker-compose exec app php artisan key:generate
 
 docker-compose exec app php artisan migrate
 
+docker-compose exec app npm install
+
+docker-compose exec app npm install vite
+
+echo "Ouvrez un terminal dans le répertoir du projet, exécutez la commande 'npm run dev' puis dans un autre terminal 'php artisan serve'"
 echo "L'application se trouve sur le http://localhost:8000 et phpmyadmin sur http://localhost:8080"
